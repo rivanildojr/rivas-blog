62cb934fa5f3d1fb47f579e0e20e02a826f51fd0
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2019-09-13 03:47:35 -0300

    Start componet Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Rivanildo Júnior</h1>
+    <h2>Desenvolverdor Backend</h2>
+    <p>Gerente de Projetos na empresa jr SOFTEAM</p>
+  </div>
+)
+
+export default Profile
