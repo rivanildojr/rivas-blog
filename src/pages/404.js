038377da939e8f374d225e7e7e9471e90d499398
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2019-09-13 02:52:16 -0300

    Initial Commit

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>404</h1>
+    <p>Não tem nada :(</p>
+  </Layout>
+)
+
+export default NotFoundPage
